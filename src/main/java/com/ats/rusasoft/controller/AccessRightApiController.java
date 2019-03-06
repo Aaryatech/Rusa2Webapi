e48387f9b4e9ff8dd5007e56b77aeb15873f4776
Aaryatech <code@atsinfosoft.com>
2019-03-06 11:39:08 +0530

    ac Right API

--- src/main/java/com/ats/rusasoft/controller/AccessRightApiController.java
+++ src/main/java/com/ats/rusasoft/controller/AccessRightApiController.java
@@ -20,7 +20,7 @@ import com.ats.rusasoft.repository.AccessRight.AssignRoleDetailListRepository;
 
 @RestController
 public class AccessRightApiController {
-
+ 
 	@Autowired
 	AccessRightService accessRightService;
 
