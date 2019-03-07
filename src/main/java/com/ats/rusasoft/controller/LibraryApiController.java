a6afe6a2ea4b8c61b0557bcdcad142c0ed041c8c
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-03-07 14:04:32 +0530

    //harsha

--- src/main/java/com/ats/rusasoft/controller/LibraryApiController.java
+++ src/main/java/com/ats/rusasoft/controller/LibraryApiController.java
@@ -1,5 +1,8 @@
 package com.ats.rusasoft.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,22 +10,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ats.rusasoft.model.GetInstituteList;
 import com.ats.rusasoft.model.Institute;
 import com.ats.rusasoft.model.Librarian;
 import com.ats.rusasoft.model.Principal;
 import com.ats.rusasoft.model.UserLogin;
 import com.ats.rusasoft.mstrepo.LibrarianRepo;
+import com.ats.rusasoft.mstrepo.UserService;
 
 @RestController
 public class LibraryApiController {
 	@Autowired
 	LibrarianRepo libRepo;
 	
+	@Autowired
+	UserService  userRepo;
+	
+	
 	@RequestMapping(value = { "/saveLibrarian" }, method = RequestMethod.POST)
 	public @ResponseBody Librarian saveLibrarian(@RequestBody Librarian librarian) {
 
 		Librarian libResp = null;
-
+ 
 		try {
 
 			if (librarian.getLibrarianId() == 0) {
@@ -31,27 +40,33 @@ public class LibraryApiController {
 				System.err.println("New librarian Insert  New Principal");
 
 				UserLogin ul = new UserLogin();
+				
+				ul.setUserType(7);
+				ul.setPass("123");
+				ul.setUserName(libResp.getLibrarianName());
+				ul.setIsBlock(1);
+				ul.setRegPrimaryKey(libResp.getLibrarianId());
+				ul.setRoleId(1);
+				ul.setExInt1(1);
+				ul.setExInt2(1);
+				ul.setExVar1("NA");
+				ul.setExVar2("NA");
+				
+				userRepo.saveAndFlush(ul);
 
-				/*princi.setEmail(insResp.getEmail());
-				princi.setInstituteId(insResp.getInstituteId());
-				princi.setPhoneNo(insResp.getContactNo());
-				princi.setPrincipalId(0);
-				princi.setPrincipalName(insResp.getPrincipalName());
-				princi.setIsEnroll(0);
-				pincipalRepo.saveAndFlush(princi);
-*/
 			} else {
 				libResp = libRepo.saveAndFlush(librarian);
 
 				System.err.println("Old  librarian Old   User  update if any");
-
-				/*Principal princi = pincipalRepo.findByInstituteId(insResp.getInstituteId());
-
-				princi.setEmail(insResp.getEmail());
-				princi.setInstituteId(insResp.getInstituteId());
-				princi.setPhoneNo(insResp.getContactNo());
-				princi.setPrincipalName(insResp.getPrincipalName());
-				pincipalRepo.saveAndFlush(princi);*/
+				/*
+				 * Principal princi = pincipalRepo.findByInstituteId(insResp.getInstituteId());
+				 * 
+				 * princi.setEmail(insResp.getEmail());
+				 * princi.setInstituteId(insResp.getInstituteId());
+				 * princi.setPhoneNo(insResp.getContactNo());
+				 * princi.setPrincipalName(insResp.getPrincipalName());
+				 * pincipalRepo.saveAndFlush(princi);
+				 */
 
 			}
 
@@ -63,6 +78,21 @@ public class LibraryApiController {
 		return librarian;
 
 	}
-
-
+	/*
+	 * @RequestMapping(value = { "/getAllLibrarian" }, method = RequestMethod.GET)
+	 * public @ResponseBody List<Librarian> getAllInstitutes() {
+	 * 
+	 * List<Librarian> insResp = new ArrayList<>();
+	 * 
+	 * try { insResp = libRepo.getAllLibList();
+	 * 
+	 * } catch (Exception e) {
+	 * System.err.println("Exce in getAllInstitutes Institute " + e.getMessage());
+	 * e.printStackTrace(); }
+	 * 
+	 * return insResp;
+	 * 
+	 * }
+	 * 
+	 */
 }
--- src/main/java/com/ats/rusasoft/mstrepo/LibrarianRepo.java
+++ src/main/java/com/ats/rusasoft/mstrepo/LibrarianRepo.java
@@ -1,12 +1,20 @@
 package com.ats.rusasoft.mstrepo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ats.rusasoft.model.GetInstituteList;
 import com.ats.rusasoft.model.Librarian;
 
 
 public interface LibrarianRepo extends JpaRepository<Librarian, Integer> {
 	
-	
+	@Query(value=" SELECT institute_id, institute_name,principal_name,email,contact_no from m_institute,t_user_login WHERE is_active=1 "
+			+ " AND del_status=1 AND t_user_login.reg_primary_key=institute_id  ", nativeQuery=true)
+		
+		List<GetInstituteList> getAllLibList();
+
 
 }
