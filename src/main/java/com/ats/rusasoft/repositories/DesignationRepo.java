69e1790701ebc165f887d2a98818a40488344e15
maddy <maddy@maddy-Inspiron-3541>
2019-03-07 12:51:13 +0530

    //Rusa api

---
+++ src/main/java/com/ats/rusasoft/repositories/DesignationRepo.java
@@ -0,0 +1,14 @@
+package com.ats.rusasoft.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.ats.rusasoft.model.Designation;
+
+
+public interface DesignationRepo extends JpaRepository<Designation, Integer> {
+
+	List<Designation> findByDelStatusAndIsActive(@Param("i") int i, @Param("j") int j);
+}
---
+++ src/main/java/com/ats/rusasoft/repositories/IqacRepo.java
@@ -0,0 +1,28 @@
+package com.ats.rusasoft.repositories;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ats.rusasoft.model.MIqac;
+
+public interface IqacRepo extends JpaRepository<MIqac, Integer> {
+
+	MIqac findByIqacIdAndDelStatus(@Param("id") int id,@Param("i") int i);
+
+	List<MIqac> findByIsActiveAndDelStatus(@Param("i") int i,@Param("j") int j);
+
+	
+	@Transactional
+	@Modifying
+	@Query(value="UPDATE m_iqac SET del_status=0 WHERE iqac_id=:id",nativeQuery=true)
+	int deleteByIqacId(@Param("id") int id);
+
+	
+
+}
