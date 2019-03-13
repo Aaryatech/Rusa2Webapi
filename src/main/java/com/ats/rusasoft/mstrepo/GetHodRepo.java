fa620364aae8f1361c23865b94b7c0e398da6fb6
Aaryatech <code@atsinfosoft.com>
2019-03-13 18:46:04 +0530

    ..Sac

--- src/main/java/com/ats/rusasoft/mstrepo/GetHodRepo.java
+++ src/main/java/com/ats/rusasoft/mstrepo/GetHodRepo.java
@@ -13,7 +13,7 @@ public interface GetHodRepo extends JpaRepository<GetHod, Integer> {
 	@Query(value=" SELECT m_hod.hod_id,m_hod.institute_id,m_hod.dept_id,m_hod.hod_name,m_hod.contact_no,m_hod.email,"
 			+ " m_dept.dept_name FROM  m_hod,m_dept WHERE m_hod.dept_id=m_dept.dept_id AND "
 			+ "  m_hod.del_status=1 and m_hod.is_active=1 and "
-			+ " m_hod.institute_id=:instId ", nativeQuery=true)
+			+ " m_hod.institute_id=:instId ORDER BY m_hod.hod_id DESC ", nativeQuery=true)
 	
 	List<GetHod> getHodListByInstId(@Param("instId") int instId);
 	
