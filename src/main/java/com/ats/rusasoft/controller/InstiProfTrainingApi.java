74914c3311d86c6befebde39b0e51b6e0d058398
Aaryatech <code@atsinfosoft.com>
2019-03-21 15:36:40 +0530

    getIns.>Sac

--- src/main/java/com/ats/rusasoft/controller/InstiProfTrainingApi.java
+++ src/main/java/com/ats/rusasoft/controller/InstiProfTrainingApi.java
@@ -1,13 +1,20 @@
 package com.ats.rusasoft.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ats.rusasoft.instprofilerepo.GetInstTrainTeachDetailRepo;
 import com.ats.rusasoft.instprofilerepo.InstituteTrainingRepo;
+import com.ats.rusasoft.model.GetAccOfficer;
+import com.ats.rusasoft.model.instprofile.GetInstTrainTeachDetail;
 import com.ats.rusasoft.model.instprofile.InstituteTraining;
 
 @RestController
@@ -30,5 +37,46 @@ public class InstiProfTrainingApi {
 
 		return instTrainRes;
 	}
+	
+	//GetInstTrainTeachDetailRepo
+	
+	@Autowired GetInstTrainTeachDetailRepo getInstTrainTeachDetailRepo;
+	
+	@RequestMapping(value = { "/getInstTrainTeachDetailList" }, method = RequestMethod.POST)
+	public @ResponseBody List<GetInstTrainTeachDetail> getInstTrainTeachDetailList(@RequestParam int instId,
+			@RequestParam int yearId,@RequestParam int trainningType) {
 
+		List<GetInstTrainTeachDetail> instTrainTeachList = new ArrayList<GetInstTrainTeachDetail>();
+
+		try {
+			
+			instTrainTeachList = getInstTrainTeachDetailRepo.getInstTrainTeachDetail(instId, yearId, trainningType);
+
+		} catch (Exception e) {
+			
+			System.err.println("Exce in getInstTrainTeachDetailList  " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return instTrainTeachList;
+	}
+
+	//get this For Edit show selected values..
+	@RequestMapping(value = { "/getInstTrainTeachDetailById" }, method = RequestMethod.POST)
+	public @ResponseBody GetInstTrainTeachDetail getInstTrainTeachDetailById(@RequestParam int trainingId) {
+
+		GetInstTrainTeachDetail instTrainTeachList = new GetInstTrainTeachDetail();
+
+		try {
+			
+			instTrainTeachList = getInstTrainTeachDetailRepo.getInstTrainTeachDetailById(trainingId);
+
+		} catch (Exception e) {
+			
+			System.err.println("Exce in getInstTrainTeachDetailById  " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return instTrainTeachList;
+	}
 }
--- src/main/java/com/ats/rusasoft/repo/faculty/GetFacPerDetailRepo.java
+++ src/main/java/com/ats/rusasoft/repo/faculty/GetFacPerDetailRepo.java
@@ -12,7 +12,7 @@ import com.ats.rusasoft.model.faculty.GetFacPerDetail;
 public interface GetFacPerDetailRepo extends JpaRepository<GetFacPerDetail, Integer> {
 	@Query(value="SELECT m_faculty.faculty_id,m_faculty.faculty_first_name AS faculty_name,m_faculty.contact_no,m_faculty.email,m_designation.designation_name,TIMESTAMPDIFF(Month,m_faculty.joining_date,:curDate) as current_experience " + 
 			"	 ,m_faculty.f_past_exp " + 
-			" FROM m_designation,m_faculty WHERE m_designation.designation_id=m_faculty.current_designation_id\n" + 
+			" FROM m_designation,m_faculty WHERE m_faculty.f_aadhar!='NULL' AND m_designation.designation_id=m_faculty.current_designation_id\n" + 
 			" AND m_faculty.institute_id=:instId AND m_faculty.del_status=1 AND m_faculty.is_active=1", nativeQuery=true)
 	List<GetFacPerDetail> getGetFacPerDetail(@Param("instId") int instId, @Param("curDate") Date curDate);
 
