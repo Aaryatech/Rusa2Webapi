b7271367c7604b9f67155ffdf2b3de60a825085f
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-03-21 18:22:37 +0530

    //neha

---
+++ src/main/java/com/ats/rusasoft/mstrepo/LinkageMasterRepo.java
@@ -0,0 +1,9 @@
+package com.ats.rusasoft.mstrepo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ats.rusasoft.model.LinkageMaster;
+
+public interface LinkageMasterRepo extends JpaRepository<LinkageMaster, Integer> {
+
+}
