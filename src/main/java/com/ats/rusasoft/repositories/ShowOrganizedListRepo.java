7ecf15d661a984f81b032d6d9d2d50193e0a9788
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-03-24 20:10:36 +0530

    //maddy

--- src/main/java/com/ats/rusasoft/repositories/ShowOrganizedListRepo.java
+++ src/main/java/com/ats/rusasoft/repositories/ShowOrganizedListRepo.java
@@ -12,7 +12,7 @@ import com.ats.rusasoft.model.OrganizedList;
 public interface ShowOrganizedListRepo extends JpaRepository<OrganizedList, Integer> {
 	
 	@Modifying
-	@Query(value="SELECT a.activity_id, o.type_name , a.activity_name, a.activity_level, a.activity_date, a.activity_participants, a.activity_funded_by, a.activity_amount_sanctioned,a.activity_amount_utilised FROM t_faculty_activity a, m_outreach_type o WHERE o.type_id=a.activity_type AND a.del_status=:del AND a.year_id=:yrId AND a.faculty_id=:facId",nativeQuery=true)
+	@Query(value="SELECT a.activity_id, o.type_name , a.activity_name, a.activity_level, a.activity_date, a.activity_participants, a.activity_funded_by, a.activity_amount_sanctioned,a.activity_amount_utilised FROM t_faculty_activity a, m_outreach_type o WHERE o.type_id=a.activity_type AND a.del_status=:del AND a.year_id=:yrId AND a.faculty_id=:facId Order By a.activity_id Desc",nativeQuery=true)
  	List<OrganizedList> getOrganizedDetailList(@Param("facId") int facId, @Param("yrId") int yrId, @Param("del") int del);
 
 }
