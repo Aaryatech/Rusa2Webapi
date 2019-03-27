1a8a21870b840147a91bb30bb7836bdc4eaeff94
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-03-27 15:27:05 +0530

    //neha

---
+++ src/main/java/com/ats/rusasoft/budgetrepo/AcademicBudgetRepo.java
@@ -0,0 +1,28 @@
+package com.ats.rusasoft.budgetrepo;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ats.rusasoft.model.budget.AcademicBudget;
+
+public interface AcademicBudgetRepo extends JpaRepository<AcademicBudget, Integer> {
+
+	AcademicBudget findByDelStatusAndIsActiveAndAcademicBudgetId(int i, int j, int academicBudgetId);
+
+	List<AcademicBudget> findByInstituteIdAndAcYearIdAndDelStatusAndIsActive(int instId, int acYearId, int i, int j);
+
+	AcademicBudget findByDelStatusAndIsActiveAndFinYearId(int i, int j, int curFinYear);
+	
+	
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE t_academic_budget SET del_status=0 WHERE academic_budget_id IN (:academicIdList) ", nativeQuery = true)
+	int deleteAcademic(@Param("academicIdList") List<String> academicIdList);
+
+}
