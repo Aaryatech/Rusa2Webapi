04ba28e837505600e388f5ee8330bd2351b19218
Aaryatech <code@atsinfosoft.com>
2019-03-27 16:41:40 +0530

    lib Book..Sac

---
+++ src/main/java/com/ats/rusasoft/budgetrepo/LibraryBookBudgetRepo.java
@@ -0,0 +1,11 @@
+package com.ats.rusasoft.budgetrepo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ats.rusasoft.model.budget.LibraryBookBudget;
+
+public interface LibraryBookBudgetRepo extends JpaRepository<LibraryBookBudget, Integer>{
+	
+	
+
+}
