d5c5eca91c7c86ee700f9937949d44bc387d4556
Aaryatech <code@atsinfosoft.com>
2019-04-12 18:34:57 +0530

    new Dean @Param.Sac

--- src/main/java/com/ats/rusasoft/repositories/NewDeanListRepo.java
+++ src/main/java/com/ats/rusasoft/repositories/NewDeanListRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ats.rusasoft.model.NewDeanList;
 
@@ -14,7 +15,7 @@ public interface NewDeanListRepo extends JpaRepository<NewDeanList, Integer> {
 			+ "WHERE f.dept_id=d.dept_id AND des.designation_id=f.current_designation_id AND "
 			+ "q.qualification_id=f.highest_qualification AND f.del_status=1 AND f.is_active=1 AND f.is_dean=:isDean "
 			+ "AND f.institute_id=:instituteId ORDER BY f.faculty_id DESC",nativeQuery=true)
-	List<NewDeanList> getDeanByInst(int instituteId, int isDean);
+	List<NewDeanList> getDeanByInst(@Param("instituteId") int instituteId, @Param("isDean") int isDean);
 
 	@Query(value="SELECT f.faculty_id,f.faculty_first_name,f.dept_id,f.highest_qualification,f.teaching_to,"
 			+ "f.current_designation_id,f.joining_date,f.realiving_date,f.contact_no,f.email,f.maker_user_id,"
@@ -22,7 +23,7 @@ public interface NewDeanListRepo extends JpaRepository<NewDeanList, Integer> {
 			+ "WHERE f.dept_id=d.dept_id AND des.designation_id=f.current_designation_id AND "
 			+ "q.qualification_id=f.highest_qualification AND f.del_status=1 AND f.is_active=1 AND f.is_dean=:isDean "
 			+ "AND d.dept_id IN(:deptIdList) AND f.institute_id=:instituteId ORDER BY f.faculty_id DESC",nativeQuery=true)
-	List<NewDeanList> getDeanByDept(List<Integer> deptIdList, int instituteId, int isDean);
+	List<NewDeanList> getDeanByDept(@Param("deptIdList")List<Integer> deptIdList, @Param("instituteId") int instituteId, @Param("isDean") int isDean);
 
 			@Query(value="SELECT f.faculty_id,f.faculty_first_name,f.dept_id,f.highest_qualification,f.teaching_to,"
 				+ "f.current_designation_id,f.joining_date,f.realiving_date,f.contact_no,f.email,f.maker_user_id,"
@@ -30,6 +31,6 @@ public interface NewDeanListRepo extends JpaRepository<NewDeanList, Integer> {
 				+ "WHERE f.dept_id=d.dept_id AND des.designation_id=f.current_designation_id AND "
 				+ "q.qualification_id=f.highest_qualification AND f.institute_id=:instituteId AND f.del_status=1 AND f.is_active=1 AND f.is_dean=:isDean "
 				+ "AND f.institute_id=:instituteId AND m_faculty.faculty_id=:facultyId ORDER BY f.faculty_id DESC",nativeQuery=true)
-	List<NewDeanList> getDeanRepo(int facultyId, int instituteId, int isDean);
+	List<NewDeanList> getDeanRepo(@Param("facultyId")int facultyId,  @Param("instituteId") int instituteId, @Param("isDean") int isDean );
 
 }
