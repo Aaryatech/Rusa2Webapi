498ff80a65d3df6626c6eef2f70bce6e5a1ac2b9
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-04-15 18:27:46 +0530

    //maddy

--- src/main/java/com/ats/rusasoft/repositories/NewDeanListRepo.java
+++ src/main/java/com/ats/rusasoft/repositories/NewDeanListRepo.java
@@ -13,24 +13,24 @@ public interface NewDeanListRepo extends JpaRepository<NewDeanList, Integer> {
 			+ "f.current_designation_id,f.joining_date,f.realiving_date,f.contact_no,f.email,f.maker_user_id,"
 			+ "d.dept_name,q.qualification_name,des.designation_name FROM m_faculty f, m_designation des,m_qualificatoin q ,m_dept d "
 			+ "WHERE f.dept_id=d.dept_id AND des.designation_id=f.current_designation_id AND "
-			+ "q.qualification_id=f.highest_qualification AND f.del_status=1 AND f.is_active=1 AND f.is_dean=:1 "
-			+ "AND f.institute_id=:instituteId ORDER BY f.faculty_id DESC",nativeQuery=true)
+			+ "q.qualification_id=f.highest_qualification AND f.del_status=1 AND f.is_active=1 "
+			+ "AND f.institute_id=:instituteId AND f.is_dean=1 ORDER BY f.faculty_id DESC",nativeQuery=true)
 	List<NewDeanList> getDeanByInst(@Param("instituteId") int instituteId);
 
 	@Query(value="SELECT f.faculty_id,f.faculty_first_name,f.dept_id,f.highest_qualification,f.teaching_to,"
 			+ "f.current_designation_id,f.joining_date,f.realiving_date,f.contact_no,f.email,f.maker_user_id,"
 			+ "d.dept_name,q.qualification_name,des.designation_name FROM m_faculty f, m_designation des,m_qualificatoin q ,m_dept d "
 			+ "WHERE f.dept_id=d.dept_id AND des.designation_id=f.current_designation_id AND "
-			+ "q.qualification_id=f.highest_qualification AND f.del_status=1 AND f.is_active=1 AND f.is_dean=:1 "
-			+ "AND d.dept_id IN(:deptIdList) AND f.institute_id=:instituteId ORDER BY f.faculty_id DESC",nativeQuery=true)
+			+ "q.qualification_id=f.highest_qualification AND f.del_status=1 AND f.is_active=1  "
+			+ "AND d.dept_id IN(:deptIdList) AND f.institute_id=:instituteId AND f.is_dean=1 ORDER BY f.faculty_id DESC",nativeQuery=true)
 	List<NewDeanList> getDeanByDept(@Param("deptIdList")List<Integer> deptIdList, @Param("instituteId") int instituteId);
 
 			@Query(value="SELECT f.faculty_id,f.faculty_first_name,f.dept_id,f.highest_qualification,f.teaching_to,"
 				+ "f.current_designation_id,f.joining_date,f.realiving_date,f.contact_no,f.email,f.maker_user_id,"
 				+ "d.dept_name,q.qualification_name,des.designation_name FROM m_faculty f, m_designation des,m_qualificatoin q ,m_dept d "
 				+ "WHERE f.dept_id=d.dept_id AND des.designation_id=f.current_designation_id AND "
-				+ "q.qualification_id=f.highest_qualification AND f.institute_id=:instituteId AND f.del_status=1 AND f.is_active=1 AND f.is_dean=:1 "
-				+ "AND f.institute_id=:instituteId AND m_faculty.faculty_id=:facultyId ORDER BY f.faculty_id DESC",nativeQuery=true)
+				+ "q.qualification_id=f.highest_qualification AND f.institute_id=:instituteId AND f.del_status=1 AND f.is_active=1  "
+				+ "AND f.institute_id=:instituteId AND m_faculty.faculty_id=:facultyId AND f.is_dean=1 ORDER BY f.faculty_id DESC",nativeQuery=true)
 	List<NewDeanList> getDeanRepo(@Param("facultyId")int facultyId,  @Param("instituteId") int instituteId);
 
 }
