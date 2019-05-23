2c06cd50ee2fdcc53e59b629c744350abe84c6df
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-05-23 18:33:59 +0530

    //harsha

--- src/main/java/com/ats/rusasoftapi/controller/ReportApiController.java
+++ src/main/java/com/ats/rusasoftapi/controller/ReportApiController.java
@@ -1,6 +1,7 @@
 package com.ats.rusasoftapi.controller;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ats.rusasoftapi.model.AcademicYear;
-import com.ats.rusasoftapi.model.MExtActList;
+import com.ats.rusasoftapi.model.SettingKeyValue;
 import com.ats.rusasoftapi.model.report.FacParticipationInBodies;
 import com.ats.rusasoftapi.model.report.NoOfPrograms;
 import com.ats.rusasoftapi.mstrepo.AcademicYearRepo;
+import com.ats.rusasoftapi.mstrepo.SettingKeyValueRepo;
 import com.ats.rusasoftapi.reportrepo.FacParticipationInBodiesRepo;
 import com.ats.rusasoftapi.reportrepo.NoOfProgramsRepo;
 
 @RestController
 public class ReportApiController {
 
+	@Autowired
+	SettingKeyValueRepo settingKeyValueRepo;
+	
 	@Autowired
 	AcademicYearRepo academicYearRepo;
 
@@ -33,9 +38,12 @@ public class ReportApiController {
 	public @ResponseBody List<NoOfPrograms> getNoOfProgramsList(@RequestParam int instId) {
 
 		List<NoOfPrograms> progList = new ArrayList<>();
-
+		SettingKeyValue setKey=new SettingKeyValue();
 		try {
-			progList = getNoOfProgramsRepo.getNoOfPrograms(instId);
+			setKey=settingKeyValueRepo.findBySettingKeyAndDelStatus("Report1",1);
+			System.err.println("stk ids :"+setKey.toString());
+			String stkId=setKey.getStringValue();
+			progList = getNoOfProgramsRepo.getNoOfPrograms(instId,stkId);
 			System.err.println("List=" + progList);
 
 		} catch (Exception e) {
